9845252894b6394f6a1ebcaa67182da870e7f919
ririko <ririmohu25@gmail.com>
2020-04-03 04:58:40 +0900

    check1実装

---
+++ check/check1.py
@@ -0,0 +1,9 @@
+x = 3 
+
+def calc(x):
+    x += 4
+    return x
+
+print(x)
+print(calc(x))
+print(x)
---
+++ check/check3.py
@@ -0,0 +1,9 @@
+b = [3]
+def calc(b):
+    b = [4]
+    return b
+
+print(b)
+print(calc(b))
+print(b)
+
