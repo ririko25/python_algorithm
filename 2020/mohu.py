a95cc46c604158865667f26bde28d7225f6543a1
ririko <ririmohu25@gmail.com>
2020-11-29 01:33:00 +0900

    eratosthenes.pyの実装

--- 2020/mohu.py
+++ 2020/mohu.py
@@ -1,17 +1,26 @@
 import sys
 
-n = input('もふもふにしたい人の名前を入力してね: ')
-
-if n.isdecimal():
-    print("stringで入力してください")
-    sys.exit()
-
-
 def mohu(n):
     hoge = n + "もっふもふ"
     return hoge
 
-result = mohu(n)
+def huwa(n):
+    hoge = n + "ふわふわ"
+    return hoge
 
-print(result)
- 
+n1 = input('もふもふにしたい人の名前を入力してね: ')
+n2 = input('ふわふわにしたい人の名前を入力してね: ')
+
+if n1.isdecimal():
+    print("stringで入力してください")
+    sys.exit()
+
+if n2.isdecimal():
+    print("stringで入力してください")
+    sys.exit()
+
+result1 = mohu(n1)
+result2 = huwa(n2)
+
+print(result1)
+print(result2)
