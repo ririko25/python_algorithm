cb382449ec3622a524f83c8a194cdfff66810945
ririko <ririmohu25@gmail.com>
2020-12-01 00:38:21 +0900

    is_leap_year

---
+++ 2020/uruu.py
@@ -0,0 +1,11 @@
+
+def is_leap_year(year):
+    if year % 4 == 0:
+        if year % 100 == 0 and year % 400 != 0:
+            return False
+        return True
+    return False
+
+for i in range(1950,2050):
+    if is_leap_year(i) == True:
+        print(i)
